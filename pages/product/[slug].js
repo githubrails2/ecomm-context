757159162788bc41be7a53352ff7ac0ae37c5907
Bill White <soniclinuxknight@hotmail.com>
2022-04-24 10:49:33 -0500

    added Buy Now functionality

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -11,9 +11,14 @@ import { useState } from "react";
 import {useStateContext} from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
+	
 	const { image, name, details, price } = product;
 	const [index, setIndex] = useState(0);
-	const {decQty,incQty,qty,onAdd} = useStateContext()
+	const { decQty, incQty, qty, onAdd,setShowCart } = useStateContext()
+	const handleBuyNow = () => {
+		onAdd(product, qty);
+		setShowCart(true)
+	}
 	return (
 		<div>
 			<div className="product-detail-container">
@@ -58,7 +63,7 @@ const ProductDetails = ({ product, products }) => {
 							<span className="minus" onClick={decQty}>
 								<AiOutlineMinus />
 							</span>
-							<span className="num" onClick={()=>{}}>
+							<span className="num" >
 								{qty}
 							</span>
 							<span className="plus" onClick={incQty}>
@@ -70,7 +75,7 @@ const ProductDetails = ({ product, products }) => {
 						<button className="add-to-cart" onClick={()=>onAdd(product,qty)}>
 							Add To Cart
 						</button>
-						<button className="buy-now" onClick={()=>{}}>
+						<button className="buy-now" onClick={handleBuyNow}>
 							Buy Now
 						</button>
 					</div>
